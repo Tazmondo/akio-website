3bc9692a4abbabcf9a282071b772f8b30c310f5b
Pedro Lourenco <pl156176@gmail.com>
2022-02-20 19:00:40 +0000

    Created components folder and basic folder structure

--- akio-website/src/App.tsx
+++ akio-website/src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  RouteComponentProps
+} from "react-router-dom";
+import HomePage from './Components/home-page/HomePage';
 
-function App() {
+
+
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+          <div className="App">
+            <Route>
+              <HomePage />
+            </Route>
+          </div>
+    </Router>
   );
 }
 
---
+++ akio-website/src/Components/home-page/HomePage.tsx
@@ -0,0 +1,11 @@
+function HomePage(): JSX.Element {
+    return (
+        <div>
+            <h1 className = 'text-center'>
+                Akio Website
+            </h1>
+        </div>
+    );
+}
+
+export default HomePage;
