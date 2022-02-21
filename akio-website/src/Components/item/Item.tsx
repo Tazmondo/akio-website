5fe12586c4e1321e1a76069d6e8639a0d6417340
Tazmondo <fondcoolman@outlook.com>
2022-02-21 17:09:30 +0000

    extract Item component parameters into an ItemProps variable

--- akio-website/src/Components/item/Item.tsx
+++ akio-website/src/Components/item/Item.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
-function Item({name, image, price, stock} : {name: string, image: string, price: number, stock: number}) {
+// Is this the right way to do this?
+// It looks neater than putting it in the function definition at least
+type ItemProps = {
+    name: string,
+    image: string,
+    price: number,
+    stock: number
+}
+
+function Item({name, image, price, stock} : ItemProps) {
     // TODO
 }
