1f8384655a2610edb62e45c38936cbaa6c0134bd
Tazmondo <fondcoolman@outlook.com>
2022-02-21 23:08:54 +0000

    add instagram to bottom right of homepage

--- akio-website/src/Components/social-media/SocialMedia.tsx
+++ akio-website/src/Components/social-media/SocialMedia.tsx
@@ -3,7 +3,7 @@ import {ReactComponent as IgLogo} from './iglogo.svg'
 
 function SocialMedia() {
     return (
-        <div className="social-media">
+        <div className="social-media position-absolute d-flex justify-content-end" style={{right: "24px", bottom:"24px"}}>
             <a id="instagram" href="https://instagram.com/akiostreetwear?utm_medium=copy_link" target="_blank">
                 <IgLogo/>
             </a>
