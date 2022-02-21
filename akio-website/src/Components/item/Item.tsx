1d1c3e2bfbe467cfcfa2f1bb90d18d12b5d097f1
Tazmondo <fondcoolman@outlook.com>
2022-02-21 17:41:20 +0000

    Item component skeleton finished

--- akio-website/src/Components/item/Item.tsx
+++ akio-website/src/Components/item/Item.tsx
@@ -2,6 +2,9 @@ import React from "react";
 
 // Is this the right way to do this?
 // It looks neater than putting it in the function definition at least
+// But u need to rewrite all the parameters again in the function definition
+//
+// Also should we have hover to see back of clothing?
 type ItemProps = {
     name: string,
     image: string,
@@ -10,5 +13,15 @@ type ItemProps = {
 }
 
 function Item({name, image, price, stock} : ItemProps) {
-    // TODO
+    // TODO - needs css
+
+    return <div>
+        <img src={image} alt={name}/>
+        <p>{name}</p>
+        <p>{price}</p>
+        <p>{stock}</p>
+    </div>
 }
+
+export { Item };
+export type { ItemProps };
