6b0b520b79b6cdff3612275d6b6601af5601269c
Tazmondo <fondcoolman@outlook.com>
2022-02-22 20:40:16 +0000

    use fixed instead of absolute position (woops)

--- akio-website/src/Components/social-media/SocialMedia.tsx
+++ akio-website/src/Components/social-media/SocialMedia.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {ReactComponent as IgLogo} from './iglogo.svg'
 
+// Use CSS instead? styling is fairly light so probably neater to leave it here
+const iconStyle = {marginLeft: "8px"}
+
 function SocialMedia(): JSX.Element {
     return (
-        <div className="social-media position-absolute d-flex justify-content-end" style={{right: "24px", bottom:"24px"}}>
-            <a id="instagram" href="https://instagram.com/akiostreetwear?utm_medium=copy_link" target="_blank">
+        <div className="social-media position-fixed d-flex justify-content-end" style={{right: "20px", bottom:"16px"}}>
+            <a id="instagram" href="https://instagram.com/akiostreetwear?utm_medium=copy_link" target="_blank" style={iconStyle}>
                 <IgLogo/>
             </a>
         </div>
