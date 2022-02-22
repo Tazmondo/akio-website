4a38dda3fe3cee5f793d9ba698d1bc40989d6d86
Tazmondo <fondcoolman@outlook.com>
2022-02-22 22:51:37 +0000

    initial backend stuff

--- flask_server/__init__.py
+++ flask_server/__init__.py
@@ -1,15 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+
+# Will uncomment when needed
+# from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 
 
 app = Flask(__name__)
 cors = CORS(app)
+
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'test_key'
 app.config['JSON_SORT_KEYS'] = False
 
+# Does it matter that i use sqlite?
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////temp/test.db'
+db = SQLAlchemy(app)
 
 from flask_server import views
 from flask_server import models
--- flask_server/models.py
+++ flask_server/models.py
@@ -1,1 +1,13 @@
 '''Database Tables'''
+
+from flask_server import db
+
+
+class User(db.Model):
+    pass
+
+
+class Item(db.Model):
+    pass
+
+
