f9c937a91c542cf36d370772b7f067905df87759
Pedro Lourenco <pl156176@gmail.com>
2022-02-22 22:28:00 +0000

    Changed navbar text and slogan text colour. Increased size of instagram icon and changed its colour

--- akio-website/src/Components/social-media/SocialMedia.tsx
+++ akio-website/src/Components/social-media/SocialMedia.tsx
@@ -3,13 +3,14 @@ import { AiFillInstagram } from "react-icons/ai"
 
 
 // Use CSS instead? styling is fairly light so probably neater to leave it here
-const iconStyle = {marginLeft: "8px"}
+const iconStyle = {marginLeft: "8px", color: 'red'}
+const iconColourStyle = {color :   "linear-gradient(blue, red)"}
 
 function SocialMedia(): JSX.Element {
     return (
         <div className="social-media position-fixed d-flex justify-content-end" style={{right: "20px", bottom:"16px"}}>
             <a className="text-reset" id="instagram" href="https://instagram.com/akiostreetwear?utm_medium=copy_link" target="_blank" style={iconStyle}>
-                <AiFillInstagram/>
+                <AiFillInstagram className = 'social-media-icon' color = "white" />
             </a>
 
         </div>
