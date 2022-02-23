ec9752a5c391124ce8af00a52ff1a932733937f1
Pedro Lourenco <pl156176@gmail.com>
2022-02-23 22:22:00 +0000

    Edited _redirects file

--- akio-website/src/Components/admin-page/AdminPage.tsx
+++ akio-website/src/Components/admin-page/AdminPage.tsx
@@ -2,13 +2,12 @@ import {useEffect} from 'react';
 
 
 function loginRequest(){
-    {}
 }
 
 
 
 function AdminPage(){
-    
+
     useEffect(loginRequest, []);
     //allows them to access the database when the website is deployed
 
