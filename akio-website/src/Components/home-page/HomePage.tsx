a25ae38b7d5a8f30fee9849bf25d3e8e29a90ae5
pedro123456789-wq <75386625+pedro123456789-wq@users.noreply.github.com>
2022-02-25 10:43:27 +0000

    testing neltify

--- akio-website/src/Components/home-page/HomePage.tsx
+++ akio-website/src/Components/home-page/HomePage.tsx
@@ -13,7 +13,7 @@ function HomePage(): JSX.Element {
             <NavBar />
 
             <h1 className = 'main-text'>
-                Akio
+                Akioooooo
             </h1>
 
             <h2 className = 'main-slogan'>
