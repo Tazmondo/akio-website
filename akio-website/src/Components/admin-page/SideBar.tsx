5852eae938e1be6f4b4143c9a289a6e1a3838870
Pedro Lourenco <75386625+pedro123456789-wq@users.noreply.github.com>
2022-03-02 08:27:48 +0000

    formatted sidebar file

--- akio-website/src/Components/admin-page/SideBar.tsx
+++ akio-website/src/Components/admin-page/SideBar.tsx
@@ -14,26 +14,26 @@ import { NavLink } from 'react-router-dom';
 function SideBar() {
     return (
         <div style = {{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
-            <CDBSidebar textColor="#fff" backgroundColor="#333">
-                <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+            <CDBSidebar textColor='#fff' backgroundColor='#333'>
+                <CDBSidebarHeader prefix={<i className='fa fa-bars fa-large'></i>}>
                     {/* TODO: change to link component */}
-                    <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
+                    <a href='/' className='text-decoration-none' style={{ color: 'inherit' }}>
                         Admin Dashboard
                     </a>
                 </CDBSidebarHeader>
 
-                <CDBSidebarContent className="sidebar-content">
+                <CDBSidebarContent className='sidebar-content'>
                     <CDBSidebarMenu>
-                        <NavLink to = "/">
-                            <CDBSidebarMenuItem icon = "columns">Items</CDBSidebarMenuItem>
+                        <NavLink to = '/'>
+                            <CDBSidebarMenuItem icon = 'columns'>Items</CDBSidebarMenuItem>
                         </NavLink>
                         
-                        <NavLink to = "/profile">
-                            <CDBSidebarMenuItem icon="user">Admin Profiles</CDBSidebarMenuItem>
+                        <NavLink to = '/profile'>
+                            <CDBSidebarMenuItem icon = 'user'>Admin Profiles</CDBSidebarMenuItem>
                         </NavLink>
                         
-                        <NavLink to = "/analytics">
-                            <CDBSidebarMenuItem icon="chart-line">Sales</CDBSidebarMenuItem>
+                        <NavLink to = '/analytics'>
+                            <CDBSidebarMenuItem icon = 'chart-line'>Sales</CDBSidebarMenuItem>
                         </NavLink>
                     </CDBSidebarMenu>
                 </CDBSidebarContent>
