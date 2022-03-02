bca5a72967f8d06b49cd94128e304c91691be5d9
Tazmondo <fondcoolman@outlook.com>
2022-03-02 21:33:39 +0000

    init itempage

---
+++ akio-website/src/Components/item-page/ItemPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {ItemProps} from "../item/Item";
+
+function ItemPage({name, frontImage, backImage, price, stock} : ItemProps): JSX.Element {
+
+
+    return <></>
+}
