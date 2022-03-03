05161d85edf8d5728d2b4c12b429951c40a2eaba
Tazmondo <fondcoolman@outlook.com>
2022-03-03 20:17:09 +0000

    add handling for whether items have been fetched or not

--- akio-website/src/Components/item-page/ItemPage.tsx
+++ akio-website/src/Components/item-page/ItemPage.tsx
@@ -4,14 +4,27 @@ import {globalItemsContext} from "../item-context/ItemContext";
 
 function ItemPage(): JSX.Element {
     let params = useParams()
-    let itemContext = useContext(globalItemsContext).itemNameMap
+    let itemContextGlobal = useContext(globalItemsContext);
+    let itemContext = itemContextGlobal.itemNameMap
     let item = itemContext[params.name as string]
 
-    return <div className="container">
-        <div className="row">
-            <h1>{item.name}</h1>
-        </div>
-    </div>
+    if (itemContextGlobal.status !== "fetching") {
+        if (item !== undefined || itemContextGlobal.status === "failed") {
+            return <div className="container">
+                <div className="row">
+                    <h1>{item.name}</h1>
+                </div>
+            </div>
+        } else {
+            return <div className="alert alert-danger" role="alert">
+                Error, item not found!
+            </div>
+        }
+    } else {
+        // todo: return a placeholder until items are fetched
+        return <div className="alert alert-warning" role="alert">
+            Fetching item...
+        </div>    }
 }
 
 export default ItemPage
