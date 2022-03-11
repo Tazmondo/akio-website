46ec3733fe5c1b2a759e67d9f234bb0ecf9d9aa2
Pedro Lourenco <75386625+pedro123456789-wq@users.noreply.github.com>
2022-03-11 08:38:52 +0000

    Added buttons to toggle between account cereation and deletion in admin page

--- akio-website/src/Components/admin-page/AdminAccounts.tsx
+++ akio-website/src/Components/admin-page/AdminAccounts.tsx
@@ -1,9 +1,26 @@
+import {useState} from 'react';
+
+
 function AdminAccounts() {
+    const [operation, setOperation] = useState('add');
+
     return (
         <div>
             <h1 className = 'text-center'>
                 Admin Accounts
             </h1>
+
+            <div className = 'item-buttons-div'>
+                <button className = {`mt-5 mr-3 admin-border admin-button ${operation === 'add' && 'selected'}`} 
+                        onClick = {() => setOperation('add')}>
+                    Create Admin
+                </button>
+
+                <button className = {`mt-5 mr-3 admin-border admin-button ${operation === 'delete' && 'selected'}`} 
+                        onClick = {() => setOperation('delete')}>
+                    Delete Admin
+                </button>
+            </div>
         </div>
     );
 }
