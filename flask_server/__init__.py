0d71dfb2dbeae0b7e22acba82be77b1b60a1921e
Tazmondo <fondcoolman@outlook.com>
2022-03-22 17:04:52 +0000

    disable CORS in production since flask server and react front end will be on same domain
    idk whether having it enabled poses any security risks or anything but better safe than sorry i guess?

--- flask_server/__init__.py
+++ flask_server/__init__.py
@@ -12,7 +12,17 @@ app.config['SECRET_KEY'] = environ['SECRET_KEY']  # Set environment variable
 app.config['JSON_SORT_KEYS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
-cors = CORS(app, supports_credentials = True)
+devMode = False
+
+if app.config['ENV'] == 'development':
+    devMode = True
+
+
+if devMode:  # In production, assume that api and front-end at same url.
+             # Therefore CORS config not needed
+    cors = CORS(app, supports_credentials = True)
+
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
