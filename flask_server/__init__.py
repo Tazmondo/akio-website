e74bbd3ea164d987cad3736cb9e1f2e3d8c8a703
Tazmondo <fondcoolman@outlook.com>
2022-03-22 16:52:16 +0000

    PEDRO READ THIS COMMIT AND CHANGE UR LOCAL RUN CONFIGS

--- flask_server/__init__.py
+++ flask_server/__init__.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True
+# app.config['DEBUG'] = True  Set FLASK_ENV to 'development' instead of setting this
 app.config['SECRET_KEY'] = environ['SECRET_KEY']  # Set environment variable
 app.config['JSON_SORT_KEYS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
