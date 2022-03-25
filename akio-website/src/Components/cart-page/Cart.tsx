2e53fabe2dffd13a66a0bcfab01f72f15f95d426
Pedro Lourenco <75386625+pedro123456789-wq@users.noreply.github.com>
2022-03-25 08:39:53 +0000

    Display items stored in cart

--- akio-website/src/Components/cart-page/Cart.tsx
+++ akio-website/src/Components/cart-page/Cart.tsx
@@ -1,9 +1,25 @@
+import {globalItemsContext} from "../item-context/ItemContext";
+import React, {useContext} from 'react'
+
+
 function Cart() {
+    let itemContextGlobal = useContext(globalItemsContext);
+
     return (
         <div>
             <h1 className = 'text-center'>
                 Cart
             </h1>
+
+            {itemContextGlobal.cart.map((item) => {
+                return (
+                    <div className = 'text-center'>
+                        <h1>
+                            {item.name}
+                        </h1>
+                    </div>
+                )
+            })}
         </div>
     );
 }
