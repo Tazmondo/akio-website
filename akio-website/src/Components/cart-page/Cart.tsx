7740c0655edeb42522e3717253d4fa1e1440c7fe
Pedro Lourenco <75386625+pedro123456789-wq@users.noreply.github.com>
2022-03-25 10:58:04 +0000

    Add option to remove item from cart. Display total price of cart.

--- akio-website/src/Components/cart-page/Cart.tsx
+++ akio-website/src/Components/cart-page/Cart.tsx
@@ -1,9 +1,38 @@
 import {globalItemsContext} from "../item-context/ItemContext";
-import React, {useContext} from 'react'
+import React, {useContext, useState, useEffect} from 'react';
+import {ItemProps} from "../item/Item";
 
 
 function Cart() {
     let itemContextGlobal = useContext(globalItemsContext);
+    const [totalPrice, setPrice] = useState<Number>(0);
+
+
+    function calculatePrice(){
+        let totalPricePence: int = 0;
+        
+        temContextGlobal.cart.forEach((item) => {
+            totalPricePence += item.price
+        });
+
+        setPrice(totalPricePence);
+    }
+
+
+    function removeItem(item : ItemProps){
+        const cartItems: ItemProps[] = itemContext.cart
+        cartItems.splice(cartItems.indexOf(item), 1);
+
+        // update local storage when item is deleted
+        window.localStorage.setItem('cart', JSON.stringify(cartItems));
+        calculatePrice();
+    }
+
+
+
+    useEffect(calculatePrice, []);
+
+
 
     return (
         <div>
@@ -16,10 +45,23 @@ function Cart() {
                     <div className = 'text-center'>
                         <h1>
                             {item.name}
+
+                            <div>
+                                <button onClick = {() => removeItem(item)}>
+                                    Remove Item
+                                </button>
+                            </div>
                         </h1>
                     </div>
                 )
             })}
+
+            <div>
+                <p>Total: {totalPrice / 100}</p>
+                <button>
+                    Checkout
+                </button>
+            </div>
         </div>
     );
 }
