556f9b6c5212021f74533cc24f1142cde7cbfcf7
Pedro Lourenco <75386625+pedro123456789-wq@users.noreply.github.com>
2022-03-30 08:35:11 +0100

    add order to paypal button

--- akio-website/src/Components/item-page/PayPalButton.tsx
+++ akio-website/src/Components/item-page/PayPalButton.tsx
@@ -5,7 +5,20 @@ function PaypalButon() {
     return (
         // @ts-ignore    
         <PayPalScriptProvider>
-            <PayPalButtons style={{ layout: "horizontal" }} />
+            <PayPalButtons style = {{ layout: "horizontal" }} 
+                           createOrder = {(data, actions) => {
+                                        return actions.order.create({
+                                            purchase_units: [
+                                                {
+                                                    amount: {
+                                                        value: '19.22',
+                                                        currency_code : 'GBP'
+                                                    },
+                                                },
+                                            ],
+                                        });
+                                    }}
+            />
         </PayPalScriptProvider>
     );
 }
